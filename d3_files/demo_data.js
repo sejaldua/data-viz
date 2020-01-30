af29490b78e663654a3ab0ec3249a39e558d9c2b
Sejal <sejal.dua@tufts.edu>
2020-01-30 09:07:42 -0500

    d3 demo starter files upload

---
+++ d3_files/demo_data.js
@@ -0,0 +1,8 @@
+const data = [{symbol:"A", name:"Company A", price:96.01},
+              {symbol:"B", name:"Company B", price:273.30},
+              {symbol:"C", name:"Company C", price:196.89},
+              {symbol:"D", name:"Company D", price:314.31},
+              {symbol:"E", name:"Company E", price:162.94},
+              {symbol:"F", name:"Company F", price:459.89},
+              {symbol:"G", name:"Company G", price:267.43}];
+
